a52ac91fa4d28c5227e86bbc78b808cdfb93e81f
tosup_msi <j20440@inc.kisarazu.ac.jp>
2022-05-19 21:32:56 +0900

    子コンポに入れたら動いた よくわからん

--- components/User.vue
+++ components/User.vue
@@ -1,38 +1,13 @@
 <template>
-  <div>
-    <v-btn text @cilck.native="SignUp">ユーザー作成ng</v-btn>
-  </div>
+  <section class="user">
+    <UserSubmit />
+  </section>
 </template>
 
 <script>
-import {
-  getAuth,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-} from 'firebase/auth'
-
+import UserSubmit from './UserComponents/UserSubmit.vue'
 export default {
-  data() {
-    return {
-      emailAddress: 'tosup@tosup.dev',
-      password: '1234@qwerzka',
-    }
-  },
-  mounted() {},
-  methods: {
-    SignUp: function () {
-      const auth = getAuth()
-      console.log(auth)
-      createUserWithEmailAndPassword(auth, this.emailAddress, this.password)
-        .then((userCredential) => {
-          console.log('user created')
-        })
-        .catch((error) => {
-          alert(error)
-          console.log(error)
-        })
-    },
-  },
+  components: { UserSubmit },
 }
 </script>
 
---
+++ components/UserComponents/UserSubmit.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="userSubmit">
+    <v-container>
+      <v-btn @click="signUp">とうろくいんぐ</v-btn>
+    </v-container>
+  </section>
+</template>
+
+<style>
+</style>
+
+<script>
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth'
+
+export default {
+  data() {
+    return {
+      emailAddress: 'tosup@tosup.dev',
+      password: '1234@qwerzka',
+    }
+  },
+  mounted() {},
+  methods: {
+    signUp: function () {
+      const auth = getAuth()
+      console.log(auth)
+      createUserWithEmailAndPassword(auth, this.emailAddress, this.password)
+        .then((userCredential) => {
+          console.log('user created')
+        })
+        .catch((error) => {
+          alert(error)
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
